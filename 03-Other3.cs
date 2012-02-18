998bb0abf60d820eabad3636cd5f2a0d9fd0e9f8
Ryan Macnak <rmacnak@gmail.com>
2012-02-17 19:15:27 -0500

    Changes to bootstrap on Squeak4.3Core

---
+++ 03-Other3.cs
@@ -0,0 +1,1 @@
+'From Squeak4.3 of 22 December 2011 [latest update: #11860] on 17 February 2012 at 6:45:52 pm'!!MethodDictionary methodsFor: 'as yet unclassified' stamp: 'ryan 2/17/2012 18:45'!rehashWithoutBecome	self rehash! !
