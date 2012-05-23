79f8497b04bfce3e4a34dde00d3c64f70a87c676
Ryan Macnak <rmacnak@gmail.com>
2012-05-23 16:10:08 -0700

    Script now zips up the image, changesfile and stamps indicating which commits of newspeak and nsboot were used. Avoid warning about saving first time on Cog

--- build.sh
+++ build.sh
@@ -3,3 +3,10 @@ rm -r Squeak4.3
 unzip Squeak4.3.zip
 mv Squeak4.3/* .
 ../nsvm/nsvm Squeak4.3.image 00-Bootstrap.st
+hg --cwd ../newspeak tip > newspeaktip
+hg --cwd ../nsboot tip > nsboottip
+zip "nsboot-$(date +%F)" "nsboot-$(date +%F).image" "nsboot-$(date +%F).changes" newspeaktip nsboottip
+mkdir ../bootimages
+mv "nsboot-$(date +%F).zip" ../bootimages
+rm "nsboot-$(date +%F).image" "nsboot-$(date +%F).changes" newspeaktip nsboottip
+echo Build complete
