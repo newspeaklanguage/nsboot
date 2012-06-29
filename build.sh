926b89f36a6c8ec2e8d0b6bdf6a28d0291a8e87c
Ryan Macnak <rmacnak@gmail.com>
2012-06-29 15:16:54 -0700

    Make bootstrap headless for cron/Jenkins jobs

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
+date
 wget -c http://ftp.squeak.org/4.3/Squeak4.3.zip
 rm -r Squeak4.3
 unzip Squeak4.3.zip
 mv Squeak4.3/* .
-../nsvm/nsvm Squeak4.3.image 00-Bootstrap.st
+../nsvm/nsvm -headless Squeak4.3.image 00-Bootstrap.st
 hg --cwd ../newspeak tip > newspeaktip
 hg --cwd ../nsboot tip > nsboottip
 zip "nsboot-$(date +%F)" "nsboot-$(date +%F).image" "nsboot-$(date +%F).changes" newspeaktip nsboottip
@@ -10,3 +11,4 @@ mkdir ../bootimages
 mv "nsboot-$(date +%F).zip" ../bootimages
 rm "nsboot-$(date +%F).image" "nsboot-$(date +%F).changes" newspeaktip nsboottip
 echo Build complete
+date
