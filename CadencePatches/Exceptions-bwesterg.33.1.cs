5565988841c086469bb233c9a63c151325e86dfc
Ryan Macnak <rmacnak@gmail.com>
2012-06-29 21:23:10 -0700

    Cadence changes to Smalltalk code from st_patches

---
+++ CadencePatches/Exceptions-bwesterg.33.1.cs
@@ -0,0 +1,1 @@
+'From nsboot-2011-08-03 of 3 August 2011 [latest update: #10966] on 12 June 2012 at 9:30:27 pm'!!InvalidDirectoryError methodsFor: 'accessing' stamp: 'bwesterg 5/25/2012 15:07'!messageText		(messageText == nil and: [pathName ~= nil])		ifTrue: [messageText := pathName].	^super messageText! !
