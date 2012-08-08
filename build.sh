fe40b0f48229390500324e45b6648547d154ec1b
Ryan Macnak <rmacnak@gmail.com>
2012-08-07 22:37:26 -0500

    Refactoring boot process from file-in scripts to an actual class; flatten some patches into the package mcz.

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ wget -c http://ftp.squeak.org/4.3/Squeak4.3.zip
 rm -r Squeak4.3
 unzip Squeak4.3.zip
 mv Squeak4.3/* .
-../nsvm/nsvm -headless Squeak4.3.image 00-Bootstrap.st
+../nsvm/nsvm -headless Squeak4.3.image NewspeakBootstrap.st
 hg --cwd ../newspeak tip > newspeaktip
 hg --cwd ../nsboot tip > nsboottip
 zip "nsboot-$(date +%F)" "nsboot-$(date +%F).image" "nsboot-$(date +%F).changes" newspeaktip nsboottip
