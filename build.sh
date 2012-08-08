d1ced21e0ac3d3090945dfd434d84e0da8cbca7a
Ryan Macnak <rmacnak@gmail.com>
2012-08-08 15:06:43 -0500

    Update bootstrap platform for reflection additions

--- build.sh
+++ build.sh
@@ -1,14 +1,29 @@
+#!/bin/sh
+
+IM=nsboot-`date +%Y-%m-%d`
+IMAGE=$IM.image
+CHANGES=$IM.changes
+ZIP=$IM.zip
+HEADLESS=-headless
+NSVM=../nsvm/nsvm
+
 date
+
 wget -c http://ftp.squeak.org/4.3/Squeak4.3.zip
 rm -r Squeak4.3
 unzip Squeak4.3.zip
-mv Squeak4.3/* .
-../nsvm/nsvm -headless Squeak4.3.image NewspeakBootstrap.st
+mv Squeak4.3/Squeak4.3.image $IMAGE
+mv Squeak4.3/Squeak4.3.changes $CHANGES
+
+$NSVM $HEADLESS $IMAGE NewspeakBootstrap.st
+
 hg --cwd ../newspeak tip > newspeaktip
 hg --cwd ../nsboot tip > nsboottip
-zip "nsboot-$(date +%F)" "nsboot-$(date +%F).image" "nsboot-$(date +%F).changes" newspeaktip nsboottip
+
+zip $ZIP $IMAGE $CHANGES newspeaktip nsboottip
 mkdir ../bootimages
-mv "nsboot-$(date +%F).zip" ../bootimages
-rm "nsboot-$(date +%F).image" "nsboot-$(date +%F).changes" newspeaktip nsboottip
-echo Build complete
+mv $ZIP ../bootimages
+rm $IMAGE $CHANGES newspeaktip nsboottip
+
 date
+
