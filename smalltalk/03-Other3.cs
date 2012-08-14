ddbd82639637b916ec5a226852887042fbac5ac3
Ryan Macnak <rmacnak@gmail.com>
2012-08-14 00:53:21 -0500

    Changes for addWeakSubclass:

--- smalltalk/03-Other3.cs
+++ smalltalk/03-Other3.cs
@@ -1,1 +1,1 @@
-'From Squeak4.3 of 22 December 2011 [latest update: #11860] on 17 February 2012 at 6:45:52 pm'!!MethodDictionary methodsFor: 'as yet unclassified' stamp: 'ryan 2/17/2012 18:45'!rehashWithoutBecome	self rehash! !
+'From Squeak4.3 of 22 December 2011 [latest update: #11860] on 13 August 2012 at 11:46:46 pm'!!Behavior methodsFor: 'accessing class hierarchy' stamp: 'ryan 8/13/2012 23:46'!addWeakSubclass: cls	"Ignore"! !!MethodDictionary methodsFor: 'as yet unclassified' stamp: 'ryan 2/17/2012 18:45'!rehashWithoutBecome	self rehash! !
