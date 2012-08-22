14e5d3d8e4d5a5e2ba683e6e67946837a58877fb
Ryan Macnak <rmacnak@gmail.com>
2012-08-21 22:35:04 -0400

    Patch the finalization process to avoid potential infinite recursion; adjust default path to NSVM to depend on the OS.

--- build.sh
+++ build.sh
@@ -5,7 +5,12 @@ IMAGE=$IM.image
 CHANGES=$IM.changes
 ZIP=$IM.zip
 HEADLESS=-headless
-NSVM=../nsvm/nsvm
+
+case `uname -s` in
+Linux) NSVM=/usr/lib/nsvm/nsvm;;
+Darwin) NSVM="/Applications/Newspeak Virtual Machine.app/Contents/MacOS/Newspeak Virtual Machine";;
+*) NSVM=../nsvm/nsvm;;
+esac
 
 date
 
@@ -15,7 +20,7 @@ unzip Squeak4.3.zip
 mv Squeak4.3/Squeak4.3.image $IMAGE
 mv Squeak4.3/Squeak4.3.changes $CHANGES
 
-$NSVM $HEADLESS $IMAGE NewspeakBootstrap.st
+"$NSVM" $HEADLESS $IMAGE NewspeakBootstrap.st
 
 hg --cwd ../newspeak tip > newspeaktip
 hg --cwd ../nsboot tip > nsboottip
