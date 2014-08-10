c098422e2b6126888a91c43497dcf505ff1c88ce
Ryan Macnak <rmacnak@gmail.com>
2014-08-10 16:48:29 -0700

    Make spurbuild.sh more like build.sh: default VM path, zipping up the output, etc; also change the image name to reflect that is in Spur format.

--- spurbuild.sh
+++ spurbuild.sh
@@ -1,19 +1,22 @@
 #!/bin/sh -e
 
-IM=nsboot-`date +%Y-%m-%d`
+IM=nsboot-spur-`date +%Y-%m-%d`
 IMAGE=$IM.image
 CHANGES=$IM.changes
+IMAGEPATH="../bootimages"
+ZIP=$IM.zip
 HEADLESS=-headless
-HEADLESS=
 
 USAGE="usage: `basename $0` -[h?] [-v vm] [-p path] [-u]"
-NSVM=nsspurcfvm
+NSVM=
+NOZIP=
 
 while getopts 'v:p:uh?' opt "$@"; do
 	case "$opt" in
 	v)		NSVM="$OPTARG";;
 	h)		HEADLESS="";;
 	p)		IMAGEPATH="$OPTARG";;
+	u)		NOZIP="1";;
 	\?|*)	echo $USAGE
 			echo '	boot newspeak'
 			echo '	-h: run headful, not headless'
@@ -26,6 +29,14 @@ while getopts 'v:p:uh?' opt "$@"; do
 	esac
 done
 
+if [ -z "$NSVM" ]; then
+	case `uname -s` in
+	Linux) NSVM=/usr/lib/nsvm/nsvm;;
+	Darwin) NSVM="/Applications/Newspeak Spur Virtual Machine.app/Contents/MacOS/Newspeak Virtual Machine";;
+	*) NSVM=../nsvm/nsvm;;
+	esac
+fi
+
 date
 
 cp -p Squeak4.3/Squeak4.3.3-spur.image $IMAGE
@@ -33,3 +44,21 @@ cp -p Squeak4.3/Squeak4.3.3-spur.changes $CHANGES
 
 echo "$NSVM" $HEADLESS $IMAGE NewspeakBootstrap.st
 "$NSVM" $HEADLESS $IMAGE NewspeakBootstrap.st
+
+if [ -z "$NOZIP" ]; then
+	hg --cwd ../newspeak tip > newspeaktip
+	hg --cwd ../nsboot tip > nsboottip
+
+	zip $ZIP $IMAGE $CHANGES newspeaktip nsboottip
+	mkdir -p $IMAGEPATH || true
+	mv $ZIP $IMAGEPATH
+	rm $IMAGE $CHANGES $CHANGES.old newspeaktip nsboottip
+	date # date output might be used for deployment scripts. if not, remove this line
+else
+	mkdir -p $IMAGEPATH || true
+	cp -f $IMAGE   "$IMAGEPATH/$IMAGE"
+	cp -f $CHANGES "$IMAGEPATH/$CHANGES"
+	rm $IMAGE $CHANGES $CHANGES.old
+	echo "\n"
+        echo "`pwd`/../bootimages/$IMAGE"
+fi
